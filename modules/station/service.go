d12e58c1b9a99c750954d2ddd7f346ff5fd44f26
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-06-13 23:11:29 +0700

    Added service, response, router, etc

---
+++ modules/station/service.go
@@ -0,0 +1,49 @@
+package station
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/KevinAntonioWiyonoLauw/mrt-schedules/common/client"
+)
+
+type Service interface {
+	GetAllStation() (response []StationResponse, err error)
+}
+
+type service struct {
+	client *http.Client
+}
+
+func NewService() Service {
+	return &service{
+		client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (s *service) GetAllStation() (response []StationResponse, err error) {
+	// layer service
+	url := "https://jakartamrt.co.id/id/val/stasiuns"
+
+	//hit url
+	byteResponse, err := client.DoRequest(s.client, url)
+	if err != nil {
+		return
+	}
+
+	var stations []Station
+	err = json.Unmarshal(byteResponse, &stations)
+
+	//keluarin response
+	for _, item := range stations {
+		response = append(response, StationResponse{
+			Id:   item.Id,
+			Name: item.Name,
+		})
+	}
+
+	return
+}
